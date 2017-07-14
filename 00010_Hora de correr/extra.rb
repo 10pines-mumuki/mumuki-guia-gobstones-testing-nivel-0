10bff75c42b090a49fea00ff975050c378a88d9f
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-07-14 14:31:46 -0400

    Mumuki Export on 2017-07-14 14:31:46 -0400

--- 00010_Hora de correr/extra.rb
+++ 00010_Hora de correr/extra.rb
@@ -1,3 +1,16 @@
-def ruby(file_name) 
-  puts "Resultados acá....."
+class TestPositivometro
+  
+  def test_positivo
+    puts "Para el 1 da: #{Positivometro.new.es_positivo? 1}"
+    puts "Para el -1 da #{Positivometro.new.es_positivo? -1}"
+    puts "Para el 0 da #{Positivometro.new.es_positivo? 0}"
+  end
+  
 end
+
+class Positivometro
+  def es_positivo?(un_numero)
+    un_numero >= 0
+  end
+end
+
