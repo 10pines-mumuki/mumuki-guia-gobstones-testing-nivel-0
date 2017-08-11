32695ced8a61ba1dc32940cb26a323bb27192692
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-08-11 18:16:02 -0400

    Mumuki Export on 2017-08-11 18:16:02 -0400

--- 00002_Un primer "test"/test.rb
+++ 00002_Un primer "test"/test.rb
@@ -3,15 +3,11 @@ describe "TestPositivometro" do
     TestPositivometro.new.test_positivo
   end    
   
-  #it "calls the puts" do
-   # expect(STDOUT).to receive(:puts).exactly(3).times
-    #subject
-  #end
-  
-  it "calls es_positivo?" do
-    expect_any_instance_of(Positivometro).to receive(:es_positivo?).once.with(-1)
-    expect_any_instance_of(Positivometro).to receive(:es_positivo?).once.with(0)
-
+  it "calls the puts" do
+    expect(STDOUT).to receive(:puts).exactly(3).times
     subject
   end
+
+  #TODO: See if it makes sense to add a test for es_positivo? calls
+  
 end
