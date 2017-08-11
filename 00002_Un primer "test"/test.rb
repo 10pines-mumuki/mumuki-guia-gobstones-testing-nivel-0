766822109f9e80667fd3386d29fe5156104e7a1b
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-08-11 18:11:01 -0400

    Mumuki Export on 2017-08-11 18:11:01 -0400

--- 00002_Un primer "test"/test.rb
+++ 00002_Un primer "test"/test.rb
@@ -1,5 +1,17 @@
-describe "un test" do 
-  it "it works" do
-    expect(1).to eq 1
+describe "TestPositivometro" do 
+  subject do
+    TestPositivometro.new.test_positivo
+  end    
+  
+  #it "calls the puts" do
+   # expect(STDOUT).to receive(:puts).exactly(3).times
+    #subject
+  #end
+  
+  it "calls es_positivo?" do
+    expect_any_instance_of(Positivometro).to receive(:es_positivo?).once.with(-1)
+    expect_any_instance_of(Positivometro).to receive(:es_positivo?).once.with(0)
+
+    subject
   end
 end
