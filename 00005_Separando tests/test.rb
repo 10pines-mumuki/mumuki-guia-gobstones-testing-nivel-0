f83e6d84f1919e5d9b6c719e44f8440384ae62cc
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-11-22 18:17:53 -0500

    Mumuki Export on 2017-11-22 18:17:53 -0500

--- 00005_Separando tests/test.rb
+++ 00005_Separando tests/test.rb
@@ -1,3 +1,25 @@
+describe "TestPositivometro.new.test_1_es_positivo" do 
+  
+  subject do
+    TestPositivometro.new.test_1_es_positivo
+  end    
+  
+  it "llama a es_positivo?(1)" do
+   expect_any_instance_of(Positivometro).to receive(:es_positivo?).with(1)
+    subject
+  end
+  
+  it "no falla" do
+    expect { subject }.to_not raise_error
+  end
+  
+  it "llama a asertar" do
+    expect_any_instance_of(TestPositivometro).to receive(:asertar).once
+    subject
+  end
+  
+end
+
 describe "TestPositivometro.new.test_0_no_es_positivo" do 
   
   subject do
@@ -33,28 +55,6 @@ describe "TestPositivometro.new.test_menos_1_no_es_positivo" do
   it "llama a es_positivo?(-1)" do
    expect_any_instance_of(Positivometro).to receive(:es_positivo?).with(-1)
     subject
-  end
-  
-  it "llama a asertar" do
-    expect_any_instance_of(TestPositivometro).to receive(:asertar).once
-    subject
-  end
-  
-end
-
-describe "TestPositivometro.new.test_1_es_positivo" do 
-  
-  subject do
-    TestPositivometro.new.test_1_es_positivo
-  end    
-  
-  it "llama a es_positivo?(1)" do
-   expect_any_instance_of(Positivometro).to receive(:es_positivo?).with(1)
-    subject
-  end
-  
-  it "no falla" do
-    expect { subject }.to_not raise_error
   end
   
   it "llama a asertar" do
