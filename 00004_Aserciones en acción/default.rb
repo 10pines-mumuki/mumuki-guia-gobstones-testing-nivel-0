c453c408ab842b631ccfaf0e2001aa26751fe51d
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-11-22 17:50:52 -0500

    Mumuki Export on 2017-11-22 17:50:52 -0500

--- 00004_Aserciones en acción/default.rb
+++ 00004_Aserciones en acción/default.rb
@@ -1,10 +1,1 @@
-class TestPositivometro
-  
-  def test_positivo
-    positivometro = Positivometro.new
-    puts "Para el 1 da: #{positivometro.es_positivo? 1}"
-    puts "Para el -1 da: #{positivometro.es_positivo? -1}"
-    puts "Para el 0 da: #{positivometro.es_positivo? 0}"
-  end
-  
-end
+/*...solution[1]...*/
--- 00004_Aserciones en acción/test.rb
+++ 00004_Aserciones en acción/test.rb
@@ -1,9 +1,8 @@
 describe "TestPositivometro#test_positivo" do 
+  
   subject do
     TestPositivometro.new.test_positivo
   end    
-  
-  # I feel like this is not enough because even though is what's happening, any code with an Exception would pass...
   
   it "falla debido al error en el código" do
     expect{subject}.to raise_error(RuntimeError)
