a0c83f358e1616a92cb1b95d588127f4f8dcc338
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-11-22 17:46:56 -0500

    Mumuki Export on 2017-11-22 17:46:56 -0500

--- 00003_Bugs y aserciones/extra.rb
+++ 00003_Bugs y aserciones/extra.rb
@@ -4,13 +4,4 @@ class Positivometro
   end
 end
 
-class TestPositivometro
-  
-  def test_positivo
-    positivometro = Positivometro.new
-    puts "Para el 1 da: #{positivometro.es_positivo? 1}"
-    puts "Para el -1 da: #{positivometro.es_positivo? -1}"
-    puts "Para el 0 da: #{positivometro.es_positivo? 0}"
-  end
-  
-end
+/*...solution[1]...*/
