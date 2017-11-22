07e7556758e65f8d1c35b620170ffd736ec18101
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-11-22 18:28:43 -0500

    Mumuki Export on 2017-11-22 18:28:43 -0500

--- 00006_Ejecutando nuevamente los tests/extra.rb
+++ 00006_Ejecutando nuevamente los tests/extra.rb
@@ -13,15 +13,15 @@ end
 class TestPositivometro < Test
   
   def test_0_no_es_positivo
-    asertar(!Positivometro.new.es_positivo? 0)
+    asertar(!Positivometro.new.es_positivo?(0))
   end
   
   def test_1_es_positivo
-    asertar(Positivometro.new.es_positivo? 1)
+    asertar(Positivometro.new.es_positivo?(1))
   end
   
   def test_menos_1_no_es_positivo
-    asertar(!Positivometro.new.es_positivo? -1)
+    asertar(!Positivometro.new.es_positivo?(-1))
   end
     
 end
