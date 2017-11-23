db7ab1f4ba9492a474e0ba9d015e22138611e33b
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-11-22 19:13:41 -0500

    Mumuki Export on 2017-11-22 19:13:41 -0500

--- 00011_Probando nuevos asertares/test.rb
+++ 00011_Probando nuevos asertares/test.rb
@@ -19,3 +19,70 @@ class TestPositivometro < Test
   end
 
 end
+
+
+describe "TestPositivometro.new.test_0_no_es_positivo" do 
+  
+  subject do
+    TestPositivometro.new.test_0_no_es_positivo
+  end    
+  
+  it "no falla" do
+    expect { subject }.to_not raise_error
+  end
+  
+  it "llama a es_positivo?(0)" do
+   expect_any_instance_of(Positivometro).to receive(:es_positivo?).with(0)
+    subject
+  end
+  
+  it "llama a asertar" do
+    expect_any_instance_of(TestPositivometro).to receive(:asertar).once
+    subject
+  end
+  
+end
+
+describe "TestPositivometro.new.test_menos_1_no_es_positivo" do 
+  
+  subject do
+    TestPositivometro.new.test_menos_1_no_es_positivo
+  end    
+  
+  it "no falla" do
+    expect { subject }.to_not raise_error
+  end
+  
+  it "llama a es_positivo?(-1)" do
+   expect_any_instance_of(Positivometro).to receive(:es_positivo?).with(-1)
+    subject
+  end
+  
+  it "llama a asertar" do
+    expect_any_instance_of(TestPositivometro).to receive(:asertar).once
+    subject
+  end
+  
+end
+
+describe "TestPositivometro.new.test_1_es_positivo" do 
+  
+  subject do
+    TestPositivometro.new.test_1_es_positivo
+  end    
+  
+  it "no falla" do
+    expect { subject }.to_not raise_error
+  end
+  
+  it "llama a es_positivo?(1)" do
+   expect_any_instance_of(Positivometro).to receive(:es_positivo?).with(1)
+    subject
+  end
+  
+  it "llama a asertar" do
+    expect_any_instance_of(TestPositivometro).to receive(:asertar).once
+    subject
+  end
+  
+end
