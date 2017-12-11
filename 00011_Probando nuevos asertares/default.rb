f73187e85702b031fced665c4a59a8170fadd6a3
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-12-11 17:51:42 -0500

    Mumuki Export on 2017-12-11 17:51:42 -0500

--- 00011_Probando nuevos asertares/default.rb
+++ 00011_Probando nuevos asertares/default.rb
@@ -1,5 +1,9 @@
 class TestPositivometro < Test
   
+  def cantidad_de_positivos_devuelve_la_cantidad_correcta_dada_una_lista
+    #Completar acá
+  end
+  
   def test_0_no_es_positivo
     asertar(!Positivometro.new.es_positivo?(0))
   end
