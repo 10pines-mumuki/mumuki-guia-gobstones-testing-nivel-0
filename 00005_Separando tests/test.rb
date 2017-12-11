42417087e0011eb1468e5bf0c856a1092e4fe0ae
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-12-11 18:56:34 -0500

    Mumuki Export on 2017-12-11 18:56:34 -0500

--- 00005_Separando tests/test.rb
+++ 00005_Separando tests/test.rb
@@ -5,8 +5,8 @@ describe "TestPositivometro.new.test_1_es_positivo" do
   end    
   
   it "llama a es_positivo?(1)" do
-   expect_any_instance_of(Positivometro).to receive(:es_positivo?).once.with(1)
-    TestPositivometro.new.test_1_es_positivo
+   expect_any_instance_of(Positivometro).to receive(:es_positivo?).once.with(1).and_return(true)
+    subject
   end
   
   it "no falla" do
